2be7eef377b3e89293de402ffdd357cce4377839
mercuryrucks <mercury@local-internet.ru>
2023-11-21 17:58:06 -0300

    UPD: Catalog scr issue updated

--- cypress/e2e/Secret_variables/Test_data.js
+++
@@ -1,8 +0,0 @@
-class TestData {
-    constructor() {
-        this.email = 'test549854545@test.com';
-        this.password = '1234567Test!';
-    }
-}
-
-export default new TestData();
--- cypress/support/navigation/AuthLogin.js
+++
@@ -1,7 +0,0 @@
-class AuthLogin {
-  visitHomePage() {
-    cy.visit('https://qa-opus.omniapartners.com/', {failOnStatusCode: false, timeout: 10000});
-  }
-}
-
-export default new AuthLogin();
